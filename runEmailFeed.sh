6b45a16403fe2d7b3f16876498d70c5152e287d4
Ben Wille <ben.wille@gmail.com>
2018-11-20 15:07:26 -0700

    Email Feed script with all stations

---
+++ runEmailFeed.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+cd /Users/bwille/Sites/Broadway/emails/private/cron/; php /Users/bwille/Sites/Broadway/emails/private/cron/feed.cron.php 1;
+php /Users/bwille/Sites/Broadway/emails/private/cron/feed.cron.php 2;
+php /Users/bwille/Sites/Broadway/emails/private/cron/feed.cron.php 3;
+php /Users/bwille/Sites/Broadway/emails/private/cron/feed.cron.php 4;
+php /Users/bwille/Sites/Broadway/emails/private/cron/feed.cron.php 5;
+php /Users/bwille/Sites/Broadway/emails/private/cron/feed.cron.php 6;
+#sudo chmod +x scriptpath/name.sh
+#wget 'http://localhost/RSL/public/staff/posts/news_feed.php'
