ef89251d3b7b9dc09e2500711ed70d0e55440079
Ben Wille <ben.wille@gmail.com>
2019-05-21 10:03:02 -0600

    Changed db_user to pi

--- clean/private/db_credentials.php
+++ clean/private/db_credentials.php
@@ -1,8 +1,6 @@
 <?php
 // keep database credentials in seperate file
 define( 'DB_SERVER', 'localhost' );
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'pi' );
 define( 'DB_PASS', 'mdrBXitX}2' );
 define( 'DB_NAME', 'emails' );
-
-
